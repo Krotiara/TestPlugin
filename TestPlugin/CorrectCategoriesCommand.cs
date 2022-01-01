4f8ea73dad2ee006a6845f12885fd01e4088da62
Krotiara <33466897+Krotiara@users.noreply.github.com>
2022-01-01 23:27:07 +0500

    framework 4.8 version, hello debug

--- TestPlugin/CorrectCategoriesCommand.cs
+++ TestPlugin/CorrectCategoriesCommand.cs
@@ -1,4 +1,5 @@
-﻿using Autodesk.Revit.DB;
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using System;
 using System.Collections.Generic;
@@ -8,11 +9,19 @@ using System.Threading.Tasks;
 
 namespace TestPlugin
 {
+    [Transaction(TransactionMode.Manual)]
     public class CorrectCategoriesCommand : IExternalCommand
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
-            throw new NotImplementedException();
+            Document document = commandData.Application.ActiveUIDocument.Document; //get active Autodesk revit project
+            //List<Element> a = new FilteredElementCollector(document).OfCategory(BuiltInCategory.OST_Walls).ToList(); //Get elements of catefory
+            //// element.Category.Id 
+            ////Returns the built -in category enum.
+            FilteredElementCollector allElementsInView = new FilteredElementCollector(document, document.ActiveView.Id);
+            List<Element> viewElements = allElementsInView.ToElements().ToList();
+            return Result.Succeeded;
+            //throw new NotImplementedException();
         }
     }
 }
---
+++ TestPlugin/ViewModels/CategoriesViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestPlugin.Models;
+
+namespace TestPlugin.ViewModels
+{
+    class CategoriesViewModel
+    {
+        private CategoriesHandler categoriesHandler;
+
+        public CategoriesViewModel(Document document)
+        {
+
+        }
+
+
+
+    }
+}
